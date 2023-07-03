5d363f84ab51528da08d4cdb2edfb0b393f7e94a
Abdul Islam <130133836+abdul-islam1@users.noreply.github.com>
2023-07-03 23:05:46 +0500

    kata:3 printer errors

---
+++ Printer Errors/script.js
@@ -0,0 +1,30 @@
+"use strict";
+function printerError(s) {
+  let count = 0;
+  for (let i = 0; i <= s.length; i++) {
+    let char = s[i];
+    if (
+      char === "a" ||
+      char === "b" ||
+      char === "c" ||
+      char === "d" ||
+      char === "e" ||
+      char === "f" ||
+      char === "g" ||
+      char === "h" ||
+      char === "i" ||
+      char === "j" ||
+      char === "k" ||
+      char === "l" ||
+      char === "m"
+    ) {
+      count++;
+    }
+  }
+  return `${s.length - count}/${s.length}`;
+}
+
+const str1 = "aaabbbbhaijjjm";
+const str2 = "aaaxbbbbyyhwawiwjjjwwm";
+console.log(printerError(str1));
+console.log(printerError(str2));
