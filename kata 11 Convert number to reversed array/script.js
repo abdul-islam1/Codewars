e2d972b969a609fb913a8bcadd943a7d861a70cb
Abdul Islam <130133836+abdul-islam1@users.noreply.github.com>
2023-08-28 22:28:34 +0500

    kata 11 Convert number to reversed array

---
+++ kata 11 Convert number to reversed array/script.js
@@ -0,0 +1,12 @@
+function digitize(n) {
+  const numArr = [...String(n)];
+  const revArr = [];
+
+  for (let i = 0; numArr.length; i++) {
+    revArr.push(+numArr.pop());
+  }
+  return revArr;
+}
+
+console.log(digitize(35231));
+console.log(digitize(0));
